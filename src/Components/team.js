453770778ab46d177807cc82d94e10ccd4911555
Subin Budhathoki <subinbudhathoki09@gmail.com>
2024-01-25 13:53:26 +0545

    preview

--- src/Components/team.js
+++ src/Components/team.js
@@ -1,8 +1,17 @@
-function AppTeam(){
-    return (
-        <div>
-           <h2>Team</h2>
-        </div>
-    )
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+function AppTeam() {
+  return (
+    <div>
+      <section id="teams" className="block teams-block">
+        <Container fluid>
+          <Row>
+            <Col>1 of 1</Col>
+          </Row>
+        </Container>
+      </section>
+    </div>
+  );
 }
 export default AppTeam;
