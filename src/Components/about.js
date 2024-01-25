5a994982ad1f2ba57fee5d866187fdbc7211683f
Subin Budhathoki <subinbudhathoki09@gmail.com>
2024-01-25 13:33:26 +0545

    git commit

---
+++ src/Components/about.js
@@ -0,0 +1,50 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import img1 from "../assets/images/img1.jpg";
+import ProgressBar from "react-bootstrap/ProgressBar";
+function AppAbout() {
+  const html = 80;
+  const responsive = 60;
+  const photoshop = 40;
+  return (
+    <section id="about" className="block about-block">
+      <Container fluid>
+        <div className="title-holder">
+          <h2>About Us</h2>
+          <div className="subtitle">learn more about us</div>
+        </div>
+        <Row>
+          <Col sm={6}>
+            <Image src={img1} />
+          </Col>
+          <Col sm={6}>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+            <div className="progress-block">
+              <h4>HTML/CSS/JS</h4>
+              <ProgressBar now={html} label={`${html}%`} />
+            </div>
+            <div className="progress-block">
+              <h4>RESPONSIVE</h4>
+              <ProgressBar now={responsive} label={`${responsive}%`} />
+            </div>
+            <div className="progress-block">
+              <h4>PHOTOSHOP</h4>
+              <ProgressBar now={photoshop} label={`${photoshop}%`} />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+export default AppAbout;
