2dd0ade9c4a267b36e21bfb0250c7895a518d557
Subin Budhathoki <subinbudhathoki09@gmail.com>
2024-01-26 13:50:44 +0545

    teamTestimonialsCompleted

--- src/Components/testimonial.js
+++ src/Components/testimonial.js
@@ -1,5 +1,28 @@
 import Container from "react-bootstrap/Container";
 import Carousel from "react-bootstrap/Carousel";
+var testimonialsData = [
+  {
+    id: 1,
+    name: "John Wills",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur non earum repudiandae ipsa reprehenderit quaerat, quo a cupiditate, voluptatibus voluptates, dolores nam accusantium error, modi iure itaque totam labore.",
+    designation: "Manager",
+  },
+  {
+    id: 2,
+    name: "Jasmine Perry",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, magni fugiat eveniet minus perspiciatis! Nostrum laborum maxime consequuntur repellat nam magni, quae incidunt distinctio enim itaque eligendi laboriosam, quod, ad!",
+    designation: "Accountant",
+  },
+  {
+    id: 3,
+    name: "Rocky Johnson",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium facilis optio porro omnis alias eaque corporis error est ut, reprehenderit quae asperiores illum quo voluptates debitis non. Repellat fugit, asperiores?",
+    designation: "CEO",
+  },
+];
 export default function AppTestimonials() {
   return (
     <section id="testimonials" className="testimonials-block">
@@ -9,13 +32,21 @@ export default function AppTestimonials() {
           <div className="subtitle"> what client says about us</div>
         </div>
         <Carousel>
-          <Carousel.Item>
-            <ExampleCarouselImage text="First slide" />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
+          {testimonialsData.map((testimonials) => {
+            return (
+              <Carousel.Item key={testimonials.id}>
+                <blockquote>
+                  <p>{testimonials.description}</p>
+                  <cite>
+                    <span className="name">{testimonials.name}</span>
+                    <span className="designation">
+                      {testimonials.designation}
+                    </span>
+                  </cite>
+                </blockquote>
+              </Carousel.Item>
+            );
+          })}
         </Carousel>
       </Container>
     </section>
