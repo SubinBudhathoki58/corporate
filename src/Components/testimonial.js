98708000dfe576c4afc03270ed703785974089a8
Subin Budhathoki <subinbudhathoki09@gmail.com>
2024-01-26 13:37:01 +0545

    carouselsinsertinTestimonial

--- src/Components/testimonial.js
+++ src/Components/testimonial.js
@@ -1,13 +1,23 @@
 import Container from "react-bootstrap/Container";
-export default function AppTestimonials(){
-    return(
-        <section id="testimonials" className="testimonials-block">
-            <Container fluid>
-                <div className="title-holder">
-                    <h2>Client Testimonials</h2>
-                    <div className="subtitle"> what client says about us</div>
-                </div>
-            </Container>
-        </section>
-    )
+import Carousel from "react-bootstrap/Carousel";
+export default function AppTestimonials() {
+  return (
+    <section id="testimonials" className="testimonials-block">
+      <Container fluid>
+        <div className="title-holder">
+          <h2>Client Testimonials</h2>
+          <div className="subtitle"> what client says about us</div>
+        </div>
+        <Carousel>
+          <Carousel.Item>
+            <ExampleCarouselImage text="First slide" />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+    </section>
+  );
 }
