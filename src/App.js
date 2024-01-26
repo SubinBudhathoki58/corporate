ea9917301069b6ae06c5de6156aca18c52ecbe39
Subin Budhathoki <subinbudhathoki09@gmail.com>
2024-01-26 14:12:24 +0545

    updation

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AppServices from './Components/AppServices';
 import AppWorks from './Components/works';
 import AppTeams from './Components/team';
 import AppTestimonials from './Components/testimonial';
+import AppPricing from './Components/Pricing';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <AppWorks/>
         <AppTeams/>
         <AppTestimonials/>
+        <AppPricing/>
       </main>
      
      
